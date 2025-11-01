088f68a8ebeb19b02c2c65457b307d82d9bf3527
Liemdeptrai so1 k1 <dangthanhwtf@gmail.com>
2025-11-02 00:36:59 +0700

    hoàng thành base đầu tiên cho trang

--- giaohangonline/app/views.py
+++ giaohangonline/app/views.py
@@ -2,5 +2,17 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse
 # Create your views here.
 def home(request):
-    
+    if request.user.is_authenticated:
+        customer = request.user
+        items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
+        user_not_login = "hidden" # và ẩn dk dn
+        user_login = "show" #nếu đăng nhập thì hiện chào
+    else:
+        items =[]
+        order = {'get_cart_items':0,'get_cart_total':0}
+        cartItems = order['get_cart_items']
+        user_not_login = "show" # ch đăng nhập thì hiện dk dn
+        user_login = "hidden" # và ẩn chào 
+   
     return render(request,'app/home.html')
--- giaohangonline/giaohangonline/settings.py
+++ giaohangonline/giaohangonline/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     'app'
 ]
 
@@ -64,6 +65,7 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
             ],
         },
+        
     },
 ]
 
